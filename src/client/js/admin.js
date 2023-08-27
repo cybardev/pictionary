5e6085b1f9c1112a8169cc934fbc39bba6ad1dc9
cybardev <sheikh@cybar.dev>
2023-08-27 01:57:10 -0300

    add word list editing interface

--- src/client/js/admin.js
+++ src/client/js/admin.js
@@ -9,21 +9,98 @@
 //creates conection to server url
 const SERVER_URL = "https://140.184.230.209:40608";
 
-const alpineData = {
+const $_ = (el) => document.querySelector(el);
+
+/**
+ * Logs out user when page is not visible
+ *
+ * Author: Sheikh Saad Abdullah
+ */
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState == "hidden") {
+        navigator.sendBeacon(SERVER_URL + "/logoff");
+    }
+});
+
+// data required for the admin page
+const adminData = {
     showPass: false,
-    authenticate() {
-        $.get(
-            SERVER_URL + "/authenticate",
-            $("#adminPassword").val(),
-            (correctPassword) => {
-                if (correctPassword) {
-                    openModal("addVocabArea");
-                } else {
-                    Swal.fire(
-                        "Incorrect username or passphrase.\nPlease try again."
-                    );
-                }
+    async authenticate(username, passphrase) {
+        await fetch(SERVER_URL + "/authenticate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                passphrase: passphrase,
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                window.location.replace("http://www.w3schools.com");
+            } else {
+                Swal.fire(
+                    "Incorrect username or passphrase.\nPlease try again."
+                );
             }
-        );
+        });
+    },
+};
+
+// data required for the editor page
+const editorData = {
+    // properties
+    currentWord: "",
+    currentWordImage: "",
+    currentWordAudio: "",
+    wordList: [
+        "aqq",
+        "eliey",
+        "kesalk",
+        "ki'l",
+        "l'tu",
+        "mijisi",
+        "ni'n",
+        "teluisi",
+        "wiktm",
+    ],
+
+    // methods
+    inWordList(word) {
+        return this.wordList.includes(word);
+    },
+    cancelChanges(event) {},
+    saveChanges(event) {},
+    addNewWord(event) {},
+    deleteConfirm(word, index) {
+        // double-check if user wants to remove word from list
+        swal({
+            title: `DELETE "${word}" from word list?`,
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                swal({
+                    title: `Are you SURE you want to DELETE "${word}" from word list?`,
+                    icon: "error",
+                    buttons: true,
+                    dangerMode: true,
+                }).then((willDelete) => {
+                    if (willDelete) {
+                        this.deleteWord(index);
+                    }
+                });
+            }
+        });
+    },
+    deleteWord(index) {
+        this.wordList.splice(index, 1);
+    },
+    updateImagePreview(event) {
+        this.currentWordImage = URL.createObjectURL(event.target.files[0]);
+    },
+    updateAudioPreview(event) {
+        this.currentWordAudio = URL.createObjectURL(event.target.files[0]);
     },
 };
